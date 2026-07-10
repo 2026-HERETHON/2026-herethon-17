b3195d2a586dbb2154958dec4d0316b82cec872c
llszos <llszos@naver.com>
2026-07-10 12:04:19 +0900

    ⚙️ chore: tracker URL/View/Template 틀 구성 (#9)

--- herb/tracker/views.py
+++ herb/tracker/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 
-# Create your views here.
+@login_required
+def select_view(request):
+    return HttpResponse("3.1 준비")
+
+
+@login_required
+def intensity_view(request):
+    return HttpResponse("3.2 준비")
+
+
+@login_required
+def complete_view(request):
+    return HttpResponse("3.3 준비")
+
+
+@login_required
+def edit_view(request):
+    return HttpResponse("3.4 준비")

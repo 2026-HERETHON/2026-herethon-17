044ff1e3a3411c4fbbd11bc607789ac6b58da0f4
llszos <llszos@naver.com>
2026-07-12 02:14:41 +0900

    ⚙️ chore: reports URL/View/Template 틀 구성 (#17)

--- herb/reports/views.py
+++ herb/reports/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 
-# Create your views here.
+
+@login_required
+def weekly_view(request):
+    return HttpResponse("weekly 화면 준비")
+
+
+@login_required
+def monthly_view(request):
+    return HttpResponse("monthly 화면 준비")

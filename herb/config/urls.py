95f32cf65f1a510fccd3b57be17740b82595ad77
hiseyeon <se020122@naver.com>
2026-07-14 00:42:38 +0900

    fix: diagnosis 브랜치 충돌 해결 #19

--- herb/config/urls.py
+++ herb/config/urls.py
@@ -19,10 +19,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-
-
-
-    
+    path('tracker/', include('tracker.urls')),
+    path('reports/', include('reports.urls')),
+    path('accounts/', include('accounts.urls')),
     path("diagnosis/", include("diagnosis.urls")),
 ]
